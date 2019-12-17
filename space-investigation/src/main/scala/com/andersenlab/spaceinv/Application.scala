55231038e194f41a4286141808338b4dc25598f7
rehkalainin <rkonstantin@gmail.com>
2019-12-17 16:51:41 +0200

    HicariPC

--- space-investigation/src/main/scala/com/andersenlab/spaceinv/Application.scala
+++ space-investigation/src/main/scala/com/andersenlab/spaceinv/Application.scala
@@ -1,7 +1,12 @@
 package com.andersenlab.spaceinv
+import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.{Future,Await}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application {
   def main(args: Array[String]): Unit = {
     println("Hello, space!")
+
   }
 }
